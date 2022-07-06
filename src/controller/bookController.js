beb6e66e7e4da237e697a1885b8e41bb10c9cb4a
Avijit Hazra <avijithazrabwn@gmail.com>
2022-07-06 18:14:57 +0530

    done

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -2,7 +2,8 @@ const Bookmodel = require("../models/bookModel")
 const reviewmodel=require("../models/reviewModel")
 const { isValid, isValidbody, isValidnumber,isValidString } = require("./validator")
 const mongoose=require("mongoose")
-
+const bookModel = require("../models/bookModel")
+const userModel=require("../models/userModel")
 const createbook = async function (req, res) {
     try {
         let data = req.body
@@ -14,11 +15,16 @@ const createbook = async function (req, res) {
         if (!isValid(title)) {
             return res.status(400).send({ status: false, message: "plz enter title" })
         }
+        
         if (!isValid(excerpt)) {
             return res.status(400).send({ status: false, message: "plz enter excerpt" })
         }
         if (!isValid(userId)) {
             return res.status(400).send({ status: false, message: "plz enter userId" })
+        }
+        if(!mongoose.isValidObjectId(userId)){
+            return res.status(400).send({ status: false, message: "plz enter Valid userID" })
+
         }
         if (!isValid(ISBN)) {
             return res.status(400).send({ status: false, message: "plz enter ISBN" })
@@ -41,6 +47,26 @@ const createbook = async function (req, res) {
         }
         if (!isValid(releasedAt)) {
             return res.status(400).send({ status: false, message: "plz enter releasedAt" })
+        }
+        //Title and IsBN unique checking
+        const uniqueCheck=await bookModel.findOne({$or:[{title},{ISBN}]})
+        if(uniqueCheck){
+            if(uniqueCheck.title==title){
+                return res.status(400).send({ status: false, message: "title alraedy exist" })
+            
+            }else{
+                return res.status(400).send({ status: false, message: "ISBN alraedy exist" })
+  
+            }
+        }
+
+        //userId exist or not
+        const userDetails=await userModel.findOne({_id:userId})
+        console.log(userDetails)
+            if(!userDetails){
+                return res.status(400).send({ status: false, message: "userId not exist" })
+
+            
         }
 
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,9 @@
 const express=require("express")
 const router=express.Router()
+const {createbook}=require("../controller/bookController")
 
 
-
-
+router.post("/books",createbook)
 
 
 
