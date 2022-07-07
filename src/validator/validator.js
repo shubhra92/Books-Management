ffef54c9284a6b9d416c99e026ed97faf5599195
AtifPervez <atifpervez0@gmail.com>
2022-07-07 13:46:15 +0530

    myproject2

--- src/validator/validator.js
+++ src/validator/validator.js
@@ -1,20 +1,23 @@
-const isValid=function(x){
-    if(typeof x==="undefined" || typeof x===null) return false
-    if(typeof x==="string" && x.trim().length===0 ) return false
-    return true
+
+// a function is defined to validate the data provided in the request body
+const isValid = function (value) {
+    if (typeof value === 'undefined' || value === null) return false
+    if (typeof value === 'string' && value.trim().length === 0) return false
+    return true;
 }
 
-const isvalidnumber=function(x){
-    if(typeof x==="undefined" || typeof x===null) return false
-    if(typeof x!=="number" )return false
-    return true
+const isValidTitle = function (title) {
+    return ['Mr', 'Mrs', 'Miss'].indexOf(title) !== -1
 }
 
-const isvalidString=function(x){
-    if(typeof x==="string" && x.trim().length===0) return false
-    return true  
-}
-const isvalidbody=function(x){
-    return Object.keys(x).length>0
-}
-module.exports={isValid,isvalidnumber,isvalidbody,isvalidString}
+
+// Regex(s) used for the validation of different keys
+// let nameRegex = /^[.a-zA-Z\s,-]+$/
+let nameRegex = /^[.a-zA-Z\s]+$/
+// let linkRegex = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/
+let emailRegex = /^[a-z]{1}[a-z0-9._]{1,100}[@]{1}[a-z]{2,15}[.]{1}[a-z]{2,10}$/
+let phoneRegex = /^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/
+let passRegex = /^(?=.?[A-Z])(?=.?[a-z])(?=.?[0-9])(?=.?[#?!@$%^&*-]).{8,15}$/
+
+
+module.exports = { isValid, isValidTitle,  nameRegex, emailRegex, phoneRegex, passRegex }  // --> exporting the variables defined in the module
