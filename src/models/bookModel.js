35d2c828e0696eaafefb083cf3620a0a586e748c
shubhra92 <shubhrogh765@gmail.com>
2022-07-07 14:58:29 +0530

    change

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -28,7 +28,7 @@ const bookSchema=new mongoose.Schema({
         isDeleted: {
             type:Boolean,
              default: false},
-        releasedAt: {type:String, 
+        releasedAt: {type:Date,
             required:true},
        
     
