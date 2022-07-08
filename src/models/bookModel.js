bb72063b1b157e7ca0f29bd66909d538de1c049b
Bhuvnesh Sharma <104725390+bhuvi844964@users.noreply.github.com>
2022-07-08 16:26:08 +0530

    Update bookModel.js

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -3,11 +3,15 @@ const ObjectId=mongoose.Schema.Types.ObjectId
 
 const bookSchema=new mongoose.Schema({
     
-        title: {type:String,
+        title: {
+            type:String,
              required:true,
-             unique: true},
-        excerpt: {type:String, 
-            required:true}, 
+             unique: true
+        },
+        excerpt: {
+            type:String, 
+            required:true
+        }, 
         userId: {
             type:ObjectId,
              required:true,
@@ -16,20 +20,32 @@ const bookSchema=new mongoose.Schema({
         ISBN: {
             type:String,
              required:true, 
-             unique:true},
+             unique:true
+        },
         category: {
             type:String, 
             required:true
         },
-        subcategory: [{type:String, required:true,trim:true}],
-        reviews: {type:Number, 
-            default: 0},
-        deletedAt: {type:Date}, 
+        subcategory: [{
+            type:String,
+            required:true,
+            trim:true
+        }],
+        reviews: {
+            type:Number, 
+            default: 0
+        },
+        deletedAt: {
+            type:Date
+        }, 
         isDeleted: {
             type:Boolean,
-             default: false},
-        releasedAt: {type:Date, 
-            required:true},
+             default: false
+        },
+        releasedAt: {
+            type:Date, 
+            required:true
+        },
        
     
 },{timestamps:true})
