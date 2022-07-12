e8fc4d63448b56cba1944935e8c5ae919e080030
shubhra92 <shubhrogh765@gmail.com>
2022-07-12 11:19:56 +0530

    ok

--- src/validator/validator.js
+++ src/validator/validator.js
@@ -20,7 +20,7 @@ const isValidbody=function(x){
 
 // Regex(s) used for the validation of different keys
 
-let nameRegex = /^[.a-zA-Z\s]+$/
+let nameRegex = /^[a-zA-Z\s]+$/
 let emailRegex = /^[a-z]{1}[a-z0-9._]{1,100}[@]{1}[a-z]{2,15}[.]{1}[a-z]{2,10}$/
 let phoneRegex = /^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/
 let passRegex = /^[a-zA-Z0-9!@#$%^&*]{8,15}$/
